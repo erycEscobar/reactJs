65e0890ea4df7c431537f8f7ec4670f3f221fcef
Eric Escobar <eryc@live.com.ar>
2022-08-18 23:44:01 -0300

    Desafio Item Collection

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAECMBakl3QCJpE-Lt1egf-_TzPObtVEv8",
+  authDomain: "myfirstproyect-34a6d.firebaseapp.com",
+  projectId: "myfirstproyect-34a6d",
+  storageBucket: "myfirstproyect-34a6d.appspot.com",
+  messagingSenderId: "480704879948",
+  appId: "1:480704879948:web:27943a54f1293d9d63c3e9",
+  measurementId: "G-JEWKBL24TE"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
