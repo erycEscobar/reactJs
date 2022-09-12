9904a159b3a570bf16c05f44039cda2bdcdeee12
Eric Escobar <eryc@live.com.ar>
2022-09-12 08:39:36 -0300

    Proyecto Final

--- src/components/Cart/Checkout.jsx
+++ src/components/Cart/Checkout.jsx
@@ -3,20 +3,27 @@ import { addDoc, collection, getFirestore } from 'firebase/firestore';
 import { Link } from 'react-router-dom';
 import { useCartContext } from "../CartContext";
 import './Checkout.css';
+import Loader from "../Loader";
 
 const Checkout = () => {
-    const [ name, setName ] = useState("");
-    const [ email, setEmail ] = useState("");
-    const [ tel, setTel ] = useState("");
 
     const [ checkoutDone, setCheckoutDone ] = useState(false);
     const { cart, totalPrice, clearCart } = useCartContext(); 
+    const [ loader, setLoader ] = useState(false);
+    const [ form, setForm ] = useState({});
+
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]:e.target.value,
+        })
+    }
 
     const order = {
         buyer: {
-            name: name,
-            email: email,
-            phone: tel,
+            name: form.nombre,
+            email: form.email,
+            phone: form.telefono,
         },
         items: cart.map(product => ({ 
             id: product.id, 
@@ -27,20 +34,30 @@ const Checkout = () => {
         total: totalPrice(),
     }
     
-    const handleCompra = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
+        setLoader(true);
         const db = getFirestore();
         const ordersCollection = collection(db, 'orders');
         addDoc(ordersCollection, order)
-            .then(({ id }) => swal(`Muchas gracias ${name}`, `su codigo de compra es ${id}`, "success"));
-        setCheckoutDone(true);
-        clearCart();
+            .then(({ id }) => {
+                swal(`Muchas gracias ${form.nombre}`, `su codigo de compra es ${id}`, "success")
+                setLoader(false);
+                clearCart();
+                setCheckoutDone(true);
+            });
+
+    }
+
+    if (loader) {
+        return (<Loader />)
     }
 
     if (checkoutDone) {
         return (
             <>
-                <h1>GRACIAS POR SU COMPRA {name}</h1>
+                <h1>GRACIAS POR SU COMPRA {form.nombre}</h1>
                 <Link to="/">
                     <button>
                         Volver Al Catalogo
@@ -53,20 +70,51 @@ const Checkout = () => {
     return (
         <div className="checkout">
             <h2>Ingrese sus datos para terminar su compra</h2>
-            <p>
-                Nombre : 
-                <input type="text" onChange={(e) => setName(e.target.value)}/>
-            </p>
-            <p>
-                eMail :
-                <input type="email" onChange={(e) => setEmail(e.target.value)}/>
-            </p>
-            <p>
-                Telefono : 
-                <input type="tel" onChange={(e) => setTel(e.target.value)}/>
-            </p>
-            <br />
-            <button onClick={handleCompra}>FINALIZAR COMPRA</button>
+            <form onSubmit={handleSubmit}>
+
+                <label htmlFor="nombre">
+                    Ingrese su nombre
+                </label>
+                <input 
+                    type="text" 
+                    id='nombre' 
+                    name='nombre' 
+                    value={form.nombre || ''} 
+                    onChange={handleChange} 
+                />
+
+                <br />
+
+                <label htmlFor="email">
+                    Ingrese su email : 
+                </label>
+                <input 
+                    type="email" 
+                    id='email' 
+                    name='email' 
+                    value={form.email || ''} 
+                    onChange={handleChange} 
+                />
+
+                <br />
+
+                <label htmlFor="telefono">
+                    Ingrese su numero de telefono : 
+                </label>
+                <input 
+                    type="tel" 
+                    id='telefono' 
+                    name='telefono' 
+                    value={form.telefono || ''} 
+                    onChange={handleChange} 
+                />
+
+                <br />
+
+                <button type="submit">FINALIZAR COMPRA</button>
+
+            </form>
+            
             <br />
             <Link to="/">
                 <button>
